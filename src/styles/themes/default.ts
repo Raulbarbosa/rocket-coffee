b00b2d45ba2b7f1128fd3a41c5665c37c6a63440
Raul Barbosa <barbosamartins94@gmail.com>
2023-08-21 22:08:17 -0300

    init/chore: initial setup and structuring

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,92 @@
+export const defaultTheme = {
+  colors: {
+    'yellow': '##DBAC2C',
+    'yellow-dark': '#C47F17',
+    'yellow-light': '#F1E9C9',
+
+    'purple': '#8047F8',
+    'purple-dark': '#4B2995',
+    'purple-light': '#EBE5F9',
+
+    'base-title': '#272221',
+    'base-subtitle': '#403937',
+    'base-text': '#574F4D',
+    'base-label': '#8D8686',
+    'base-hover': '#D7D5D5',
+    'base-button': '#E6E5E5',
+    'base-input': '#EDEDED',
+    'base-card': '#F3F2F2',
+
+    'background': '#FAFAFA',
+    'white': '#FFFFFF',
+  },
+
+  fonts: {
+    ballo2: {
+      'title-xl': {
+        size: '48px',
+        lineHeight: '130%',
+        wheight: 'extraBold'
+      },
+      'title-l': {
+        size: '32px',
+        lineHeight: '130%',
+        wheight: 'extraBold'
+      },
+      'title-m': {
+        size: '24px',
+        lineHeight: '130%',
+        wheight: 'extraBold'
+      },
+      'title-s': {
+        size: '20px',
+        lineHeight: '130%',
+        wheight: 'bold'
+      },
+      'title-xs': {
+        size: '18px',
+        lineHeight: '130%',
+        wheight: 'bold'
+      }
+
+    },
+
+    roboto: {
+      'text-l': {
+        size: '20px',
+        lineHeight: '130%',
+        wheight: 'regular'
+      },
+      'text-m': {
+        size: '16px',
+        lineHeight: '130%',
+        wheight: 'regular'
+      },
+      'text-s': {
+        size: '14px',
+        lineHeight: '130%',
+        wheight: 'regular'
+      },
+      'text-xs': {
+        size: '12px',
+        lineHeight: '130%',
+        wheight: 'bold'
+      },
+      'tag': {
+        size: '10px',
+        lineHeight: '130%',
+        wheight: 'bold'
+      },
+      'button-g': {
+        size: '14px',
+        lineHeight: '160%',
+        wheight: 'bold'
+      },
+      'button-m': {
+        size: '12px',
+        lineHeight: '160%',
+        wheight: 'regular'
+      },
+    }
+  }
+}
